aa4e61bf3bcbbd8b4f34537424883a25ee3a478f
Yrina <hj3416song@naver.com>
2024-05-25 11:09:36 +0900

    Settings

---
+++ src/main/java/junior_heart/diet_hub/DietHubApplication.java
@@ -0,0 +1,13 @@
+package junior_heart.diet_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietHubApplication.class, args);
+	}
+
+}
